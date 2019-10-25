870aa9815f773b46a5f36c94817b9303eefecd24
MediaGit0609 <53787988+MediaGit0609@users.noreply.github.com>
2019-10-25 13:00:35 +0300

    Comment variables

--- var.php
+++ var.php
@@ -1,13 +1,13 @@
 <?php
 // Variables
-$desired_day   = 20;
-$desired_month = 11;
-$desired_year  = 2019;
+$desired_day   = 20; // День дэдлайна
+$desired_month = 11; // Месяц дэдлайна
+$desired_year  = 2019; // Год дэдлайна
 
-$current_day   = date('d');
-$current_month = date('m');
-$current_year  = date('Y');
+$current_day   = date('d'); // Нынешний день
+$current_month = date('m'); // Нынешний месяц
+$current_year  = date('Y'); // Нынешний год
 
-$day_left      = $desired_day     - $current_day;
-$month_left    = $desired_month   - $current_month;
-$year_left     = $desired_year    - $current_year;
+$day_left      = $desired_day     - $current_day; // Осталось дней
+$month_left    = $desired_month   - $current_month; // Осталось месяцев
+$year_left     = $desired_year    - $current_year; // Осталось лет
